9c22eeae6803c3e1860a0d91f610f3b3404e94ca
miguelgonpam <86729713+miguelgonpam@users.noreply.github.com>
2025-03-26 18:27:11 +0100

    Comentar Seleccion de algoritmo, escalabilidad al encontrar la solución óptima

--- main.py
+++ main.py
@@ -61,22 +61,23 @@ class Interface:
         self.texto5 = ttk.Entry(self.appFrame)
         self.texto5.place(anchor="center", height=50, width=150, x=int(width/4)+200, y=int(height/3)+270 )
 
-        self.button1 = ttk.Button(self.appFrame, command=self.seleccion)
-        self.button1.configure(cursor="hand2", text='SOLUCIÓN SELECCIONADA', compound="top")
-        self.button1.place(anchor="s", height=50, width=190, x=int(width/4)+500, y=int(height/3)+350)
+        #COMBOX SELECCIONAR ALGORITMO
+        #self.button1 = ttk.Button(self.appFrame, command=self.seleccion)
+        #self.button1.configure(cursor="hand2", text='SOLUCIÓN SELECCIONADA', compound="top")
+        #self.button1.place(anchor="s", height=50, width=190, x=int(width/4)+500, y=int(height/3)+350)
 
         self.button2 = ttk.Button(self.appFrame, command=self.optima)
         self.button2.configure(cursor="hand2", text='SOLUCIÓN ÓPTIMA', compound="top")
         self.button2.place(anchor="s", height=50, width=380, x=int(width/4)+90, y=int(height/3)+350)
 
-
-        self.algorithmbox = ttk.Combobox(self.appFrame)#waypoint
-        funcs =[func.nombre
-                for _, func in inspect.getmembers(opt, inspect.isfunction)
-                if hasattr(func, "nombre")]
-        self.algorithmbox.configure(cursor="hand2", state="readonly",
-                                    values=tuple(funcs))
-        self.algorithmbox.place(anchor="center", height=30, width=190, x=int(width/4)+500, y=int(height/3)+270)
+        #COMBOX SELECCIONAR ALGORITMO
+        #self.algorithmbox = ttk.Combobox(self.appFrame)#waypoint
+        #funcs =[func.nombre
+        #        for _, func in inspect.getmembers(opt, inspect.isfunction)
+        #        if hasattr(func, "nombre")]
+        #self.algorithmbox.configure(cursor="hand2", state="readonly",
+        #                            values=tuple(funcs))
+        #self.algorithmbox.place(anchor="center", height=30, width=190, x=int(width/4)+500, y=int(height/3)+270)
         
 
         
@@ -92,32 +93,32 @@ class Interface:
         self.mainwindow.mainloop()
         
 
-    def seleccion(self):
-        try:
-            X=int(self.texto1.get())
-            Y=int(self.texto2.get())
-            D=int(self.texto3.get())
-            E=int(self.texto4.get())
-            S=int(self.texto5.get())
-            sel = self.algorithmbox.selection_get()
-            funciones = {
-                func.nombre: func
-                for _, func in inspect.getmembers(opt, inspect.isfunction)
-                if hasattr(func, "nombre")  # Filtrar solo las que tienen nombre personalizado
-            }
-            
-            seleccion=funciones[sel](X,Y,D,E,S)
-            coordenadas = [{"x": x, "y": y} for x, y in seleccion]
-
-            data = {"num": len(coordenadas), "coordenadas": coordenadas}
-
-            # Guardar en un archivo .json
-            with open("output.json", "w", encoding="utf-8") as file:
-                json.dump(data, file)
-
-            plot.plot_neumaticos(X,Y,D, seleccion)
-        except:
-            messagebox.showinfo('ERROR', 'Valores incorrectos, asegúrese de introducir números enteros')
+    #def seleccion(self):
+    #    try:
+    #        X=int(self.texto1.get())
+    #        Y=int(self.texto2.get())
+    #        D=int(self.texto3.get())
+    #        E=int(self.texto4.get())
+    #        S=int(self.texto5.get())
+    #        sel = self.algorithmbox.selection_get()
+    #        funciones = {
+    #            func.nombre: func
+    #            for _, func in inspect.getmembers(opt, inspect.isfunction)
+    #            if hasattr(func, "nombre")  # Filtrar solo las que tienen nombre personalizado
+    #        }
+    #        
+    #        seleccion=funciones[sel](X,Y,D,E,S)
+    #        coordenadas = [{"x": x, "y": y} for x, y in seleccion]
+    #
+    #        data = {"num": len(coordenadas), "coordenadas": coordenadas}
+    #
+    #        # Guardar en un archivo .json
+    #        with open("output.json", "w", encoding="utf-8") as file:
+    #            json.dump(data, file)
+    #
+    #        plot.plot_neumaticos(X,Y,D, seleccion)
+    #    except:
+    #        messagebox.showinfo('ERROR', 'Valores incorrectos, asegúrese de introducir números enteros')
 
 
     def optima(self):
@@ -127,11 +128,20 @@ class Interface:
             D=int(self.texto3.get())
             E=int(self.texto4.get())
             S=int(self.texto5.get())
-            resultados = [[]] * 3
-            resultados[0] = opt.distribucion_hexagonal_neumaticos(X, Y, D, E, S)
-            resultados[1] = opt.distribucion_maxima_densidad(X,Y,D,E,S)
-            resultados[2] = opt.optimizar_distribucion_neumaticos(X,Y,D,E,S)
-            #resultados[3] = opt.backtracking(X,Y,D,E,S)
+
+            funciones = [
+                func for _ ,func in inspect.getmembers(opt, inspect.isfunction)
+                if hasattr(func, "nombre")  # Filtrar solo las que tienen nombre personalizado
+            ]
+
+            resultados = [[]] * len(funciones)
+            print(funciones)
+            cont=0
+            for fun in funciones:
+                resultados[cont]=funciones[cont](X,Y,D,E,S)
+                cont+=1
+
+            print(resultados)
             btr = []
             for i in resultados:
                 if len(btr) < len(i):
@@ -139,6 +149,8 @@ class Interface:
         
             plot.plot_neumaticos(X,Y,D, btr)
             
+
+
             coordenadas = [{"x": x, "y": y} for x, y in btr]
             data = {"num": len(coordenadas), "coordenadas": coordenadas}
 
